2feddbe6e18a5bf932cdfa163d08f64bb015cbbd
juspky <11074890+juspky@users.noreply.github.com>
2025-04-25 02:14:16 +0200

    Update generate-pak5.sh

--- builder/generate-pak5.sh
+++ builder/generate-pak5.sh
@@ -10,7 +10,6 @@ https://github.com/cubiq/ComfyUI_essentials/raw/refs/heads/main/requirements.txt
 https://github.com/cubiq/ComfyUI_FaceAnalysis/raw/refs/heads/main/requirements.txt
 https://github.com/cubiq/ComfyUI_InstantID/raw/refs/heads/main/requirements.txt
 https://github.com/cubiq/PuLID_ComfyUI/raw/refs/heads/main/requirements.txt
-https://github.com/Fannovel16/comfyui_controlnet_aux/raw/refs/heads/main/requirements.txt
 https://github.com/Fannovel16/ComfyUI-Frame-Interpolation/raw/refs/heads/main/requirements-no-cupy.txt
 https://github.com/FizzleDorf/ComfyUI_FizzNodes/raw/refs/heads/main/requirements.txt
 https://github.com/Gourieff/ComfyUI-ReActor/raw/refs/heads/main/requirements.txt
